503ddd155d58eb89101295891c74f521da0e8ca3
rogilmore <thatrickgilmore@gmail.com>
2016-09-16 10:06:37 -0400

    save plots in plot.aggregate.R

--- child-laminar-radial/analyses/plot.aggregate.R
+++ child-laminar-radial/analyses/plot.aggregate.R
@@ -1,6 +1,9 @@
 # plot.aggregate.R
 # Script to make several plots based on the aggregate data.
 
+# libraries
+library(dplyr, ggplot2)
+
 # load data
 df <- read.csv(file = "analyses/data-aggregate/moco-beh-child.csv", header = TRUE)
 
@@ -16,9 +19,12 @@ df.bysub.bycond <- summarize.bysub.bycond(df)
 
 # Plot p(corr)
 p1 <- plot.p.corr(df.bysub.bycond)
+ggsave(plot = p1, filename = "analyses/img/p.corr.pdf")
 
 # Plot RTs
 p2 <- plot.rt(df.bysub.bycond)
+ggsave(plot = p2, filename = "analyses/img/rt.pdf")
 
 # Plot p(corr) by age
 p3 <- plot.p.corr.by.age.yrs(df.bysub.bycond)
+ggsave(plot = p3, filename = "analyses/img/p.corr.by.age.yrs.pdf")
